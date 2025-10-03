10116ca3a809755858b317723f1c82b399b5d414
linux44gh6 <abireshan32@gmail.com>
2025-10-04 04:15:25 +0600

    add jwt stagery and server home page

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { PrismaModule } from './module/prisma/prisma.module';
 import { MailModule } from './module/mail/mail.module';
 import { PrismaService } from './module/prisma/prisma.service';
 import { TwilioModule } from './module/twilio/twilio.module';
+import { SeederService } from './seed/seed.service';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, MailModule, TwilioModule],
+  imports: [PrismaModule, MailModule, TwilioModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService,PrismaService],
+  providers: [AppService,PrismaService,SeederService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+import { JwtGuard } from './common/guard/jwt.guard';
+import { Prisma } from '@prisma/client';
+import { RolesGuard } from './common/guard/roles.guard';
+import { PrismaService } from './module/prisma/prisma.service';
+import * as express from 'express';
+import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const public_dir=join(process.cwd(), 'public');
   // swager setup 
   const config = new DocumentBuilder()
     .setTitle('My API')
@@ -14,7 +19,11 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  SwaggerModule.setup('docs', app, document);
+   app.use('/', express.static(public_dir));
+  const reflector = app.get(Reflector);
+  const prisma = app.get(PrismaService);
+  app.useGlobalGuards(new JwtGuard(reflector, prisma), new RolesGuard(reflector));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
