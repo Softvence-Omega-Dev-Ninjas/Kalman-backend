62286594895b1998e91b0177bcf0ed5c48c509fc
Al-amin07 <alsujon2001@gmail.com>
2025-10-21 02:19:21 +0000

    sl

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ async function bootstrap() {
   );
   // app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000" ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
