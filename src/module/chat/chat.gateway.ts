32f97ceb4bf76273f6187551e7a33177bbb5214b
shahadathhs <shahadathhossensajib732@gmail.com>
2025-11-01 02:50:36 +0600

    feat; update caddyfile for socket connection

--- src/module/chat/chat.gateway.ts
+++ src/module/chat/chat.gateway.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
@@ -20,33 +21,36 @@ import { ChatService } from './chat.service';
       'http://localhost:3001',
       'http://localhost:5173',
     ],
-    methods: ['GET', 'POST', 'OPTIONS'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   },
 })
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  private logger: Logger = new Logger(ChatGateway.name);
+
   constructor(private chatService: ChatService) {}
   @WebSocketServer() server: Server;
 
   afterInit(server: Server) {
-    console.log('Chat Gateway Initialized');
+    console.log('Chat Gateway Initialized', server.adapter.name);
   }
 
-  handleConnection(client: Socket) {
+  async handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`, client.handshake);
     const userId = client.handshake.query.userId as string;
     if (!userId) {
       client.disconnect(true);
       return;
     }
-    client.join(userId);
-    console.log(`Client connected: ${client.id} (User: ${userId})`);
+    await client.join(userId);
+    this.logger.log(`Client connected: ${client.id} (User: ${userId})`);
   }
 
   handleDisconnect(client: Socket) {
     const userId = client.handshake.query.userId as string;
-    console.log(`Client disconnected: ${client.id} (User: ${userId})`);
+    this.logger.log(`Client disconnected: ${client.id} (User: ${userId})`);
   }
 
   @SubscribeMessage('send_message')
@@ -60,6 +64,7 @@ export class ChatGateway
     },
     @ConnectedSocket() client: Socket,
   ) {
+    this.logger.log(`Message received: ${JSON.stringify(data)}`, client.id);
     const { senderId, reciverId, message, file } = data;
 
     if (!message && !file) return;
