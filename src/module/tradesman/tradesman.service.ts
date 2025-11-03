2c6aa388d4d4e8b25a9799979bf06d1589fc52fd
Al-amin07 <alsujon2001@gmail.com>
2025-11-03 23:03:30 +0000

    la

--- src/module/tradesman/tradesman.service.ts
+++ src/module/tradesman/tradesman.service.ts
@@ -26,9 +26,15 @@ export class TradesmanService {
     createTradesmanDto: CreateTradesManDto,
     files: Express.Multer.File[],
   ) {
-    const { docs, businessDetail, serviceArea, paymentMethod, ...restData } =
-      createTradesmanDto;
-    console.log({ restData });
+    const {
+      docs,
+      businessDetail,
+      serviceArea,
+      paymentMethod,
+      subCategories,
+      ...restData
+    } = createTradesmanDto;
+    console.log({ createTradesmanDto });
     try {
       if (!files?.find((el) => el.fieldname === 'doc')!) {
         throw new HttpException(
@@ -62,13 +68,13 @@ export class TradesmanService {
           email: createTradesmanDto?.email,
         },
       });
+      console.log({ subCategories });
       if (isTradesManExist) {
         throw new HttpException(
           'This email is already used',
           HttpStatus.BAD_REQUEST,
         );
       }
-
       const doc = await saveFileAndGetUrl(
         files?.find((el) => el.fieldname === 'doc')!,
       );
@@ -96,6 +102,7 @@ export class TradesmanService {
       const tradesman = await this.prisma.tradesMan.create({
         data: {
           ...restData,
+          subCategories,
           userId: isUserExist?.id,
           stripeConnectId: stripeConnect.id,
 
@@ -161,7 +168,7 @@ export class TradesmanService {
           paymentMethod: true,
         },
       });
-
+      console.log({ tradesman });
       return {
         success: true,
         message: 'Tradesman created successfully',
